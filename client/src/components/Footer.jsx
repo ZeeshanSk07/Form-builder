de080146ee377d13b6fc12b88e76fe40bf082ea3
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2024-07-20 16:03:29 +0530

    Landing Page done

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import './Footer.css';
+import directto from '../assets/link.png';
 
 function Footer() {
   return (
     <>
-      
+      <div className="footer">
+        <div className="column">
+          Made with ❤️ by
+          <a href="">@cuvette</a>
+        </div>
+        <div className="column">
+          <a href="">Status <img src={directto} alt="" /></a>
+          <a href="">Documentation <img src={directto} alt="" /></a>
+          <a href="">Roadmap <img src={directto} alt="" /></a>
+          <a href="">Pricing</a>
+        </div>
+        <div className="column">
+          <a href="">Discord <img src={directto} alt="" /></a>
+          <a href="">Github repository <img src={directto} alt="" /></a>
+          <a href="">Twitter <img src={directto} alt="" /></a>
+          <a href="">LinkedIn <img src={directto} alt="" /></a>
+          <a href="">OSS Friends</a>
+        </div>
+        <div className="column">
+          <a href="">About</a>
+          <a href="">Contact</a>
+          <a href="">Terms of Service</a>
+          <a href="">Privacy Policy</a>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+import './HomePage.css';
+import headlogo from '../assets/headlogo.png';
 import Landing1 from "../components/LandingPage/Landing1";
 import Landing2 from "../components/LandingPage/Landing2";
 import Landing3 from "../components/LandingPage/Landing3";
 import Landing4 from "../components/LandingPage/Landing4";
 import Landing5 from "../components/LandingPage/Landing5";
+import Footer from "../components/Footer";
 
 function HomePage() {
   return (
     <>
+      <div className="header">
+        <div><img src={headlogo} alt='icon' /><span>FormBot</span></div>
+        <div className="userbtn">
+          <button style={{backgroundColor:"transparent",color:'rgba(126, 166, 255, 1)',border:'1px solid rgba(126, 166, 255, 1)'}}>Sign in</button>
+          <button>Create a Formbot</button>
+        </div>
+      </div>
       <Landing1 />
       <Landing2 />
       <Landing3 />
       <Landing4 />
       <Landing5 />
+      <Footer />
     </>
   );
 }
