7a3908d1d8d34ce62b2af2b4e1df0151affe2a47
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2024-07-21 12:35:10 +0530

    404 error route added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUp from "./pages/SignUp";
+import Notfound from "./pages/Notfound";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route exact path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path= '*' element={<Notfound/>} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ client/src/pages/Notfound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Notfound.css';
+
+function Notfound() {
+  return (
+    <>
+    <div className='head'>
+        <h1 className='heading'>404 Not Found</h1>
+        <p className='msg'>Sorry, the page you're looking for doesn't exist.</p>
+    </div>
+       
+    </>
+  )
+}
+
+export default Notfound
