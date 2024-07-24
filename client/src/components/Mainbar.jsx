9dd97c2dff097f3cc5277d01168fa5e0ad408739
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2024-07-25 01:34:49 +0530

    create type bot designed with navopt remained

--- client/src/components/Mainbar.jsx
+++ client/src/components/Mainbar.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './Mainbar.css';
+import start from '../assets/typebot/start.png';
 
 function Mainbar() {
   return (
     <>
         <div className="mainbar">
-            
+            <div><img src={start} alt="flag" />Start</div>
+
+            <div className='askbubble'>
+              <div>Image</div>
+              <input type="text" placeholder='Enter text' />
+            </div>
+
+            <div className='askinput'>
+              <div>Input Text</div>
+              <p>Hint : User will tap to rate out of 5</p>
+            </div>
         </div>
     </>
   )
--- client/src/pages/CreateTypebot.jsx
+++ client/src/pages/CreateTypebot.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CreateTypebot.css';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import Mainbar from '../components/Mainbar';
 
 function CreateTypebot() {
+
+  const [bubble, setBubble] = useState(false);
+  const [inputs, setInputs] = useState(false);
+  const [selected, setSelected] = useState([]);
   return (
     <>
         <div className='botmainpg'>
