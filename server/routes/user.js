e69f24558aa6bd52f4aa5a45d281fe32c81bb6d0
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2024-07-31 17:31:10 +0530

    try

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,8 +4,8 @@ const { login, signup, Updateuser} = require('../controller/usercontroller');
 const { newTheme, GetTheme, SetTheme} = require('../controller/themecontroller');
 
 
-router.post('/log', login());
-router.post('/register', signup());
+router.post('/log', login);
+router.post('/register', signup);
 router.post('/theme', newTheme());
 
 router.put('/update/:id', Updateuser());
